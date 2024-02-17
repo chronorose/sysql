c3e3f29808054e47957121f73adb4c18190757c2
chronorose <rendemare@gmail.com>
2024-02-17 17:42:27 +0300

    added some templates, trying to write some pagess

--- src/pager.cpp
+++ src/pager.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+enum class PgType {
+    Leaf,
+    Node  
+};
+
 class FileHeader {
     long num_;
     long root_;
@@ -10,8 +15,8 @@ class FileHeader {
     public:
     FileHeader() {
         this->num_ = 0;
-        this->root_ = 32;
-        this->free_ = 0;
+        this->root_ = 0;
+        this->free_ = 32;
     }
     void setNum(long num) {
         this->num_ = num;
@@ -33,14 +38,28 @@ class FileHeader {
     }
 };
 
+template<typename T> char* toBytes(T* val) {
+    return (char*)val;
+}
+
+template<typename T> void writeBytes(ostream& os, T val) {
+    os.write(toBytes(&val), sizeof(val));
+}
+
 ostream& operator<<(ostream& os, FileHeader fhdr) {
     os.seekp(8);
-    long root = fhdr.getRoot();
-    long num = fhdr.getNum();
-    long free = fhdr.getFree();
-    os.write((char *) & root, 8);
-    os.write((char *) & num, 8);
-    os.write((char *) & free, 8);
+    // long root = fhdr.getRoot();
+    // long num = fhdr.getNum();
+    // long free = fhdr.getFree();
+    writeBytes(os, fhdr.getRoot());
+    writeBytes(os, fhdr.getNum());
+    writeBytes(os, fhdr.getFree());
+    // writeLong(os, fhdr.getRoot());
+    // writeLong(os, fhdr.getNum());
+    // writeLong(os, fhdr.getFree());
+    // os.write((char *)&root, 8);
+    // os.write((char *)&num, 8);
+    // os.write((char *)&free, 8);
     return os;
 }
 
@@ -68,24 +87,72 @@ class Pager {
     Pager(string fileName) {
         this->fileName_ = fileName;
     }
-    void open_in() {
+    void redirect(string fileName) {
+        this->fileName_ = fileName;
+    }
+    void open_read() {
         file_.open(this->fileName_, ios::binary | fstream::in);
     }
-    void open_out() {
+    void open_write() {
         file_.open(this->fileName_, ios::binary | fstream::out);
     }
     void write_initial() {
         if (!file_.is_open()) {
-            open_out();
+            open_write();
         }
-        file_.write("sysql db", 8);
+        writeBytes(file_, "sysql db");
         FileHeader fhdr;
         file_ << fhdr;
+        file_.close();
     }
+
+    FileHeader getHeader() {
+        if (!file_.is_open()) {
+            open_read();
+        }
+        FileHeader fhdr;
+        file_ >> fhdr;
+        file_.close();
+        return fhdr;
+    }
+
+    long getFreeOffset() {
+        return getHeader().getFree();
+    }
+
+    void writePage() {
+        if (!file_.is_open()) {
+            open_write();
+        }
+        // long offset = getFreeOffset();
+
+    }
+
     ~Pager() {
         file_.close();
     }
 };
+
+class Page {
+    long offset;
+    long parent;
+    PgType type;
+    long left_sibling;
+    long right_sibling;
+
+    public:
+    Page(long offset) {
+        this->offset = offset;
+        this->parent = 0;
+        this->type = PgType::Leaf;
+        this->left_sibling = 0;
+        this->right_sibling = 0;
+    }    
+};
+
+// ostream& operator<<(ostream& os, Page pg) {
+    
+// }
 
 int main() {
     Pager pager(string("Kek"));
