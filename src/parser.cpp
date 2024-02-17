504253f97580267ba13979ed5503b1fa2823920a
Reshetnikova Natalia <nataliareshetnikova943@gmail.com>
2024-02-17 18:17:06 +0700

    merge

---
+++ src/parser.cpp
@@ -0,0 +1,196 @@
+#include "Command.cpp"
+#include "lexer.cpp"
+#include <exception>
+#include <pstl/glue_execution_defs.h>
+#include <string>
+#include <system_error>
+#include <type_traits>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+enum class CommandType { Select, Insert, Create };
+
+class ParseException : exception {};
+
+class Parsed {
+public:
+  Command command;
+  CommandType type;
+};
+
+class Parser {
+private:
+  vector<Lexeme> lexemes_;
+  int len_;
+  enum class Types { Int = 1, Data, String };
+
+  void cheackSemicolon(int index) {
+    if (!(index + 1 == len_ && lexemes_[index].type == LexemeType::Semicolon)) {
+      throw ParseException();
+    }
+  }
+
+  Command parseSelect() {
+    Select select = Select();
+    int i = 1;
+    for (; i + 1 < len_ && lexemes_[i].type == LexemeType::Identifier; i++) {
+      select.selectedFields.push_back(lexemes_[i++].value);
+      if (lexemes_[i].type != LexemeType::Comma) {
+        break;
+      }
+    }
+    if (select.selectedFields.size() == 0) {
+      throw ParseException();
+    }
+    if (i + 2 < len_ && lexemes_[i].type == LexemeType::From &&
+        lexemes_[i + 1].type == LexemeType::Identifier) {
+      select.table = lexemes_[i + 1].value;
+    } else {
+      throw ParseException();
+    }
+    cheackSemicolon(i);
+    return select;
+  }
+
+  // Command parseInsert() {
+  //   Insert insert = Insert();
+  //   int i = 1;
+  //   if (!(i < len_ && lexemes_[i++].type == LexemeType::Into)) {
+  //     throw ParseException();
+  //   }
+  //   if (i < len_ && lexemes_[i].type == LexemeType::Identifier) {
+  //     insert.table = lexemes_[i++].value;
+  //   } else {
+  //     throw ParseException();
+  //   }
+  //   if (!(i < len_ && lexemes_[i++].type == LexemeType::LeftParen)) {
+  //     throw ParseException();
+  //   }
+  //   for (; i + 1 < len_ && lexemes_[i].type == LexemeType::Identifier; i++) {
+  //     Record record;
+  //     record.name = lexemes_[i++].value;
+  //     insert.records.push_back(record);
+  //     if (lexemes_[i].type != LexemeType::Comma) {
+  //       break;
+  //     }
+  //   }
+  //   if (!(i + 2 < len_ && lexemes_[i++].type == LexemeType::RightParen &&
+  //         lexemes_[i++].type == LexemeType::Values &&
+  //         lexemes_[i++].type == LexemeType::LeftParen)) {
+  //     throw ParseException();
+  //   }
+  //   int j = 0;
+  //   for (; i + 1 < len_ && lexemes_[i].type == LexemeType::Identifier; i++) {
+  //     if (j >= insert.records.size()) {
+  //       throw ParseException();
+  //     }
+  //     insert.records[j++].value = lexemes_[i++].value;
+  //     if (lexemes_[i].type != LexemeType::Comma) {
+  //       break;
+  //     }
+  //   }
+  //   if (!(j == insert.records.size() && i < len_ &&
+  //         lexemes_[i++].type == LexemeType::RightParen)) {
+  //     throw ParseException();
+  //   }
+  //   cheackSemicolon(i);
+  //   return insert;
+  // }
+
+  Command parseCreate() {
+    Create create = Create();
+    int i = 1;
+    if (i >= len_) {
+      throw ParseException();
+    }
+    switch (lexemes_[i++].type) {
+    case LexemeType::Database:
+      if (i < len_ && lexemes_[i].type == LexemeType::Identifier) {
+        create.object = Object::DataBase;
+        create.name = lexemes_[i++].value;
+      } else {
+        throw ParseException();
+      }
+      break;
+    case LexemeType::Table:
+      if (i < len_ && lexemes_[i].type == LexemeType::Identifier) {
+        create.object = Object::Table;
+        create.name = lexemes_[i++].value;
+      } else {
+        throw ParseException();
+      }
+      if (!(i < len_ && lexemes_[i++].type == LexemeType::LeftParen)) {
+        throw ParseException();
+      }
+      for (; i + 2 < len_ && lexemes_[i].type == LexemeType::Identifier &&
+             lexemes_[i + 1].type == LexemeType::Identifier;
+           i++) {
+        auto lexemeName = lexemes_[i++];
+        auto lexemeType = lexemes_[i++];
+        Field field;
+        if (lexemeType.value == "Int") {
+          field.type = Type::Int;
+        } else if (lexemeType.value == "Data") {
+          field.type = Type::Data;
+        } else if (lexemeType.value == "String") {
+          field.type = Type::String;
+        } else {
+          throw ParseException();
+        }
+        field.name = lexemeName.value;
+        create.fields.push_back(field);
+        if (lexemes_[i].type != LexemeType::Comma) {
+          break;
+        }
+      }
+      if (!(i < len_ && lexemes_[i++].type == LexemeType::RightParen)) {
+        throw ParseException();
+      }
+      break;
+    default:
+      throw ParseException();
+    }
+    cheackSemicolon(i);
+    return create;
+  }
+
+public:
+  Parser(Lexer lexer) {
+    lexer.lex();
+    lexemes_ = lexer.lexemes;
+    len_ = lexemes_.size();
+  }
+
+  Parsed parse() {
+    Parsed parsed = Parsed();
+    if (len_ == 0) {
+      throw ParseException();
+    }
+    switch (lexemes_[0].type) {
+    case LexemeType::Select:
+      parsed.command = parseSelect();
+      parsed.type = CommandType::Select;
+      break;
+    // case LexemeType::Insert:
+    //   parsed.command = parseInsert();
+    //   parsed.type = CommandType::Insert;
+    //   break;
+    case LexemeType::Create:
+      parsed.command = parseCreate();
+      parsed.type = CommandType::Create;
+      break;
+    default:
+      throw ParseException();
+    }
+    return parsed;
+  }
+};
+
+int main() {
+ Lexer lexer = Lexer();
+ Parser parser = Parser(lexer);
+ Parsed parsed = parser.parse();
+ cout << (int)parsed.type << "\n";
+}
