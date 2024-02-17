573e3b7578ab4276d663c560dac5e99ae558f32d
hitogava <a.putintsev@g.nsu.ru>
2024-02-17 11:08:10 +0700

    Hello lexer

---
+++ lexer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Lexer {
+    public:
+        Lexer() {
+            cout << "Hello from Lexer" << endl;
+        }
+};
+
+int main() {
+    Lexer lexer;
+    return 0;
+}
