94e447fb233ca2b00c03259a02ac512dd75ff8ac
hitogava <a.putintsev@g.nsu.ru>
2024-02-17 17:51:38 +0700

    Identifiers/Keywords

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -23,7 +24,7 @@ enum class LexemeType {
     Select, From, Create, Table, Database,
 
 
-    None
+    Eof, None,
 };
 
 map<string, LexemeType> keywords {
@@ -49,8 +50,18 @@ string LexemeTypeToString(LexemeType type) {
             return "Database";
         case LexemeType::Create:
             return "Create";
+
+        case LexemeType::Comma:
+            return "Comma";
+        case LexemeType::Dot:
+            return "Dot";
+        case LexemeType::Semicolon:
+            return "Semicolon";
+
         case LexemeType::None:
             return "None";
+        case LexemeType::Eof:
+            return "Eof";
         default:
             return "Type not in enum";
     }
@@ -77,96 +88,140 @@ class Lexeme {
 
 class Lexer {
     public:
-
         size_t cursor;
+        size_t start;
         string inputBuffer;
 
         vector<Lexeme> lexemes;
-        void addLexeme(LexemeType type, string value) {
-            Lexeme lexeme(type, value);
+
+        void addLexeme(Lexeme lexeme) {
             lexemes.push_back(lexeme);
         }
 
-        bool isEnd() { return cursor == inputBuffer.length(); }
+        bool isEnd() { return cursor >= inputBuffer.length(); }
+        void moveCursor() {
+            cursor++;
+        }
         bool isKeyword(string key) {
             return keywords.find(key) != keywords.end();
         }
-        void moveCursor() { cursor++; }
-        char current() {
+        char advanceCursor() {
+            return inputBuffer[cursor++];
+        }
+        char peek() {
             return isEnd() ? '\0' : inputBuffer.at(cursor);
         }
-        void lex() {
-            while (getline(cin, inputBuffer)) {
-                while (!isEnd()) {
-                    string buffer = "";
-                    if (isspace(current())) {
-                        moveCursor();
-                        continue;
-                    }
-                    switch(current()) {
-                        case '*':
-                            addLexeme(LexemeType::Star, "*");
-                            moveCursor();
-                            break;
-                        case '.':
-                            addLexeme(LexemeType::Dot, ".");
-                            moveCursor();
-                            break;
-                        case ',':
-                            addLexeme(LexemeType::Comma, ",");
-                            moveCursor();
-                            break;
-                        case '(':
-                            addLexeme(LexemeType::LeftParen, "(");
-                            moveCursor();
-                            break;
-                        case ')':
-                            addLexeme(LexemeType::RightParen, ")");
-                            moveCursor();
-                            break;
-                        case '{':
-                            addLexeme(LexemeType::LeftBracket, "{");
-                            moveCursor();
-                            break;
-                        case '}':
-                            addLexeme(LexemeType::RightBracket, "}");
-                            moveCursor();
-                            break;
-                    }
-                    if (isalpha(current())) {
-                        buffer.push_back(current());
-                        moveCursor();
-                        while (!isspace(current()) && current() != 0) {
-                            buffer.push_back(current());
-                            moveCursor();
-                        }
-                        if (isKeyword(buffer)) {
-                            addLexeme(keywords[buffer], buffer);
-                        } else {
-                            addLexeme(LexemeType::Identifier, buffer);
-                        }
-                    }
-                }
-                moveCursor();
-                printLexemes();
-                inputBuffer = "";
-                cursor = 0;
-                lexemes.clear();
+        bool match(char expected) {
+            if(isEnd()) {
+                return false;
             }
+            if (peek() != expected)
+                return false;
+            moveCursor();
+            return true;
+        }
+        void identifier() {
+            while (isalpha(peek()) || isdigit(peek()))
+                advanceCursor();
+        }
+        void skipWhitespace() {
+            for (;;) {
+                char c = peek();
+                switch (c) {
+                    case ' ':
+                    case '\r':
+                    case '\t':
+                        advanceCursor();
+                        break;
+                    case '\n':
+                        advanceCursor();
+                        break;
+                    default:
+                        return;
+                }
+        }
+    }
+        Lexeme makeLexeme(LexemeType type, string value) {
+            return Lexeme(type, value);
+        }
+        Lexeme scanLexeme() {
+            skipWhitespace();
+            start = cursor;
+            if (isEnd())
+                return makeLexeme(LexemeType::Eof, "Eof");
+            char ch = advanceCursor();
+            switch (ch) {
+                case '*':
+                    return makeLexeme(LexemeType::Star, "*");
+                case ';':
+                    return makeLexeme(LexemeType::Semicolon, ";");
+                    break;
+                case ',':
+                    return makeLexeme(LexemeType::Comma, ",");
+                case '.':
+                    return makeLexeme(LexemeType::Dot, ".");
+                default:
+                    if (isalpha(ch)) {
+                        identifier();
+                        string substring = inputBuffer.substr(start, cursor);
+                        if (!isKeyword(substring)) {
+                            return makeLexeme(LexemeType::Identifier, substring);
+                        } else {
+                            return makeLexeme(keywords[substring], substring);
+                        }
+                    }
+            }
+            return makeLexeme(LexemeType::None, "");
+        }
+        void lex() {
+            for (;;) {
+                Lexeme lexeme = scanLexeme();
+                addLexeme(lexeme);
+                if (lexeme.type == LexemeType::Eof)
+                    break;
+            }
+            char ch;
+            while ((ch = advanceCursor()) != 0) {
+                cout << ch << endl;
+        }
         }
         void printLexemes() {
             for(size_t i = 0; i < lexemes.size(); i++) {
                 lexemes.at(i).print();
             }
         }
+        Lexer(string inputBuffer) {
+            this->cursor = 0;
+            this->start = 0;
+            this->inputBuffer = inputBuffer;
+        }
         Lexer() {
+            this->start = 0;
             this->cursor = 0;
             this->inputBuffer = "";
         }
 };
+string readQueryFromFile(string filePath) {
+    ifstream fs;
+    fs.open(filePath);
+    string content;
+    char ch;
+    for ( ;; ) {
+        fs >> ch;
+        if (fs.eof())
+            break;
+        content += ch;
 
-int main() {
-    Lexer lexer;
+    }
+    fs.close();
+    return content;
+}
+
+int main(int argc, char* argv[]) {
+    string filePath = argc > 1 ? argv[1] : "src/queries.txt";
+    string query = readQueryFromFile(filePath);
+    Lexer lexer(query);
     lexer.lex();
+    lexer.printLexemes();
     return 0;
 }
